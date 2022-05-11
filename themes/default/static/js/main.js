013f16ca8a005c010c2e19825333bc96600e6e2d
FuXiaoHei <fuxiaohei@vip.qq.com>
2022-05-11 13:21:04 +0800

    feat: default theme add mobile responsive styles

--- themes/default/static/js/main.js
+++ themes/default/static/js/main.js
@@ -1,3 +1,4 @@
+// dark mode button
 var themeToggleBtn = document.getElementById("dark-mode-toggle");
 if (themeToggleBtn) {
     var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon"),
@@ -7,3 +8,10 @@ if (themeToggleBtn) {
         themeToggleDarkIcon.classList.toggle("hidden"), themeToggleLightIcon.classList.toggle("hidden"), localStorage.getItem("color-theme") ? "light" === localStorage.getItem("color-theme") ? (document.documentElement.classList.add("dark"), localStorage.setItem("color-theme", "dark")) : (document.documentElement.classList.remove("dark"), localStorage.setItem("color-theme", "light")) : document.documentElement.classList.contains("dark") ? (document.documentElement.classList.remove("dark"), localStorage.setItem("color-theme", "light")) : (document.documentElement.classList.add("dark"), localStorage.setItem("color-theme", "dark"))
     });
 }
+
+// mobile menu button
+const btn = document.getElementById("nav-mobile-button");
+const menu = document.getElementById("nav-mobile-menu");
+btn.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+});
